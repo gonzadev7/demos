c29ca94bc28bb11ef1771034427d036129e24b8c
gonzadev7 <gonzagon496@gmail.com>
2026-05-14 11:22:47 -0300

    Add scrolled header color change

--- copani-moron/script.js
+++ copani-moron/script.js
@@ -1,3 +1,8 @@
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('scrolled', window.scrollY > 60);
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
